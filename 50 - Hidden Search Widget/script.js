74614fe117fde01a45f98f10e7351903ea33a865
mofath <mohamed.elgaedi@gmail.com>
2021-03-23 20:58:14 +0200

    Add hiden search widget

---
+++ 50 - Hidden Search Widget/script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const entry = document.querySelector('.entry');
+
+btn.addEventListener('click', () => {
+    container.classList.toggle('active');
+    entry.focus;
+});
